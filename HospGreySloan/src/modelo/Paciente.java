368dcf20305faa59cb40534d748829b8755f49e8
NiO <119584115+OxidoDeNiquel@users.noreply.github.com>
2025-04-07 21:43:24 +0200

    Termino modelo Pacientes

--- HospGreySloan/src/modelo/Paciente.java
+++ HospGreySloan/src/modelo/Paciente.java
@@ -4,14 +4,48 @@
  */
 package modelo;
 
+import excepciones.EstadoException;
+
 /**
  *
  * @author niobeclaveria
  */
 public class Paciente extends Usuario{
     
-    public Paciente(int id, String nombre) {
-        super(id, nombre);
+    private int edad;
+    private EstadoPaciente estado;
+    
+    public Paciente(int id, String nombre, String apellido, int edad, String estado){
+        super(id, nombre, apellido);
+        
+        this.edad = edad;
+        
+        //Nos aseguramos de que el estado sea el correcto
+        if(estado == "ASEGURADO" || estado == "NO_ASEGURADO"){            
+            this.estado = EstadoPaciente.valueOf(estado);            
+        }
+        
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public EstadoPaciente getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoPaciente estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente:\n" + "Nombre=" + this.getNombre() + "\nApellido=" + this.getApellido() + "\nEdad=" + edad + "\nEstado=" + estado;
     }
     
 }
--- HospGreySloan/src/modelo/Usuario.java
+++ HospGreySloan/src/modelo/Usuario.java
@@ -12,10 +12,12 @@ public class Usuario {
     
     private int id;
     private String nombre;
+    private String apellido;
 
-    public Usuario(int id, String nombre) {
+    public Usuario(int id, String nombre, String apellido) {
         this.id = id;
         this.nombre = nombre;
+        this.apellido = apellido;
     }
 
     public int getId() {
@@ -32,6 +34,14 @@ public class Usuario {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 
     @Override
